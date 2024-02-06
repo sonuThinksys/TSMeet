47d662ffdf79d06acf7cb4509ea54bf16a6973c0
patelsonu905 <spatel@dwellworks.com>
2024-02-06 12:56:04 +0530

    setup project TSMeet

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import JoinScreen from './screens/JoinScreen';
+
+function App() {
+  return (
+    <div className="App">
+     <JoinScreen/>
+    </div>
+  );
+}
+
+export default App;
